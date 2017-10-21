5f60672287f00c5e8157a2b40967d96165203642
EnterStudios <enterstudio@users.noreply.github.com>
2017-10-21 18:46:00 +0600

    Add files via upload

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+    /**
+     * Array of application names.
+     */
+    app_name: ['shasthojoy'],
+    /**
+     * Your New Relic license key.
+     */
+    license_key: 'f8c6ca4108d2f76df3ca1013f98aae8f0ee16ae3',
+    logging: {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level: 'info'
+    }
+}
